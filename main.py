3cd0f8622a60654d272d4ac31823129a24919b52
TheDD97 <pntdnc97h28d976l@studenti.unical.it>
2022-04-29 17:09:48 +0200

    operator always on listening

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from numpy import true_divide
 import speech_recognition as sr
 import pyttsx3
 import datetime
@@ -11,75 +12,103 @@ import wolframalpha
 import json
 import requests
 
+started = False
 engine = pyttsx3.init('sapi5')
+print(engine.getProperty('rate'))
+engine.setProperty('rate',140)
 voices = engine.getProperty('voices')
-engine.setProperty('voice',voices[0].id)
+engine.setProperty('voice',voices[3].id)
 for voice in engine.getProperty('voices'):
     print(voice.id)
+
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
 def wishMe():
+    morning = "Buon giorno"
+    afternoon = "Buon pomeriggio"
+    evening = "Buona sera"
     hour = datetime.datetime.now().hour
     if hour >= 0 and hour < 12:
-        speak("Ciao, Buon giorno")
-        print("Ciao, Buon giorno")
+        speak(morning)
+        print(morning)
     elif hour >= 12 and hour < 18:
-        speak("Ciao, Buon pomeriggio")
-        print("Ciao, Buon pomeriggio")
+        speak(afternoon)
+        print(afternoon)
     else:
-        speak("Ciao, Buona sera")
-        print("Ciao, Buona sera")
+        speak(evening)
+        print(evening)
 
-def takeCommand():
+def startJarvis():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("In ascolto....")
+        print("Eccomi sir....")
         audio=r.listen(source)
-
         try:
-            statement = r.recognize_google(audio,language='it-IT')
-            print(f"hai detto:{statement}\n")
+            initialize = r.recognize_google(audio,language='it-IT')
+            print(f"hai detto:{initialize}\n ditemi")
         except  Exception as e:
-            speak("Mi dispiace ma non ho capito.... potresti ripetere?")
             return "None"
-        return statement
+        return initialize
 
-print("Loading")
-speak("Loading")
-#speak("Signore..... mi sto avviando")
-#wishMe()
+def takeCommand():
+    if(started):
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("In ascolto....")
+            audio=r.listen(source, timeout=5)
+            try:
+                statement = r.recognize_google(audio,language='it-IT')
+                print(f"hai detto:{statement}\n")
+            except  Exception as e:
+                speak("Non ho capito.... potresti ripetere?")
+                return "None"
+            return statement
+    return "None"
+
+speak("...Signore..... mi sto avviando")
 
 if __name__ == '__main__':
     while True:
-        speak("Che cosa posso fare per te?")
-        statement = takeCommand().lower()
-        if statement == 0:
-            continue
-        if "good bye" in statement or "ok bye" in statement or "stop" in statement:
-            speak('your personal assistant G-one is shutting down,Good bye')
-            print('your personal assistant G-one is shutting down,Good bye')
-            break
-        if 'wikipedia' in statement:
-            speak('Searching Wikipedia...')
-            statement =statement.replace("wikipedia", "")
-            results = wikipedia.summary(statement, sentences=3)
-            speak("According to Wikipedia")
-            print(results)
-            speak(results)
-        elif 'open youtube' in statement:
-            webbrowser.open_new_tab("https://www.youtube.com")
-            speak("youtube is open now")
-            time.sleep(5)
-        elif 'open google' in statement:
-            webbrowser.open_new_tab("https://www.google.com")
-            speak("Google chrome is open now")
-            time.sleep(5)
-        elif 'open gmail' in statement:
-            webbrowser.open_new_tab("gmail.com")
-            speak("Google Mail open now")
-            time.sleep(5) 
-        elif 'time' in statement:
-            strTime=datetime.datetime.now().strftime("%H:%M:%S")
-            speak(f"the time is {strTime}")
+        initialize = startJarvis().lower()
+        if("jarvis" in initialize):
+            started = True
+        while started:
+            speak("Che cosa posso fare per te?")
+            statement = takeCommand().lower()
+            if statement == 0:
+                continue
+            if "basta" in statement or "annulla" in statement or "chiudi" in statement or "stop" in statement:
+                speak('OK')
+                print('your personal assistant G-one is shutting down,Good bye')
+                started = False
+                break
+            # if 'wikipedia' in statement:
+            #     speak('lo cerco su Wikipedia...')
+            #     statement = statement.replace("wikipedia", "")
+            #     results = wikipedia.summary(statement, sentences=3)
+            #     speak("According to Wikipedia")
+            #     print(results)
+            #     speak(results)
+            # el
+            if 'youtube' in statement:
+                webbrowser.open_new_tab("https://www.youtube.com")
+                speak("Avvio Youtube")
+                time.sleep(5)
+                started = False
+            elif 'apri google' in statement:
+                webbrowser.open_new_tab("https://www.google.com")
+                speak("Sto aprendo Google Chrome")
+                time.sleep(5)
+                started = False
+            elif 'open gmail' in statement:
+                webbrowser.open_new_tab("gmail.com")
+                speak("Google Mail open now")
+                time.sleep(5) 
+                started = False
+            elif 'time' in statement:
+                strTime=datetime.datetime.now().strftime("%H:%M:%S")
+                speak(f"sono le. {strTime}")
+                wishMe()
+                started = False
